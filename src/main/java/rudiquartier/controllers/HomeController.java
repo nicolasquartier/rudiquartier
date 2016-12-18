6638a32cd52043ff66b2da6cab28d4b4dabcfb0a
Nicolas Quartier <quartier.nicolas@gmail.com>
2016-12-18 15:15:53 +0100

    Removed inline script

--- src/main/java/rudiquartier/controllers/HomeController.java
+++ src/main/java/rudiquartier/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package rudiquartier.controllers;
 
-import rudiquartier.domain.ImageHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import rudiquartier.domain.ImageHolder;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,13 +18,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/")
 public class HomeController {
 
+    private static final String mainFolder = "./src/main/webapp/static/img/beelden";
+    private static final String mainFolderShort = "../../static/img/beelden";
+
     @RequestMapping(value = "/home", method = GET)
     public String home(Model model) {
         return "home";
     }
-
-    private static final String mainFolder = "./src/main/webapp/static/img/beelden";
-    private static final String mainFolderShort = "../../static/img/beelden";
 
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String index(Model model) {
@@ -34,13 +34,13 @@ public class HomeController {
 
         List<ImageHolder> thumbnails = new ArrayList<>();
         File folderBeelden = new File(mainFolder);
-        if(folderBeelden.list() != null) {
+        if (folderBeelden.list() != null) {
             for (String urlFolderSpecificBeeld : folderBeelden.list()) {
                 File folderSpecificBeeld = new File(mainFolder + "/" + urlFolderSpecificBeeld);
-                if(folderSpecificBeeld.isDirectory()) {
+                if (folderSpecificBeeld.isDirectory()) {
                     List<String> extraBeelden = new ArrayList<>();
                     for (int i = 0; i < folderSpecificBeeld.list().length; i++) {
-                        if(i == 0) continue;
+                        if (i == 0) continue;
                         extraBeelden.add(mainFolderShort + "/" + urlFolderSpecificBeeld + "/" + folderSpecificBeeld.list()[i]);
                     }
                     thumbnails.add(new ImageHolder(mainFolderShort + "/" + urlFolderSpecificBeeld + "/" + folderSpecificBeeld.list()[0], urlFolderSpecificBeeld, extraBeelden));
